25a4e064bafd881b6302c83cd47fa3e900ab8bd2
Matheus <mbkshfi@gmail.com>
2026-06-03 16:46:33 -0300

    adicionados filteredtasks e os componentes no app.jsx

--- task-dashboard/src/App.jsx
+++ task-dashboard/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,17 +6,38 @@ import './App.css'
 import useTask from './hooks/useTask'
 import TaskForm from './components/tasks/TaskForm'
 import TaskList from './components/tasks/TaskList'
+import TaskFilters from './components/tasks/TaskFilters'
 
 function App() {
-  const [count, setCount] = useState(0);
   const {tasks,addTask,removeTask,updateTask,completeTask} = useTask();
+  const [filter, setFilter] = useState("");
+
+  const filteredTasks = useMemo(() => {
+    if(filter === "pendentes"){
+      return tasks.filter(task => task.completed === false);
+    } else if(filter === "concluidas"){
+      return tasks.filter(task => task.completed === true);
+    } else if(filter === "altaprioridade"){
+      return tasks
+        .filter(task => task.completed === false)
+        .filter(task => task.priority === "alta");
+    } else if(filter === "baixaprioridade"){
+      return tasks
+        .filter(task => task.completed === false)
+        .filter(task => task.priority === "baixa");
+    } else {
+      return tasks
+    }
+  }, [tasks, filter])
 
 
   return (
     <div>
       <TaskForm addTask={addTask}/>
 
-      <TaskList tasks={tasks} completeTask={completeTask} removeTask={removeTask}/>
+      <TaskFilters filter={filter} setFilter={setFilter}/>
+
+      <TaskList tasks={filteredTasks} completeTask={completeTask} removeTask={removeTask}/>
     </div>
   )
 }
