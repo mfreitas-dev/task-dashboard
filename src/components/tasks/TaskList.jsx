8763defdbb47ea310bc6e0e45c43b9da375d58b1
Matheus <mbkshfi@gmail.com>
2026-06-10 17:44:42 -0300

    feat: updateTask integrado

--- src/components/tasks/TaskList.jsx
+++ src/components/tasks/TaskList.jsx
@@ -1,9 +1,16 @@
 import TaskCard from "./Taskcard";
 
-export default function TaskList({tasks, completeTask, removeTask}){
+export default function TaskList({tasks, completeTask, removeTask, updateTask}){
     return (
         <div>
-            {tasks.map((task) => <TaskCard task={task} key={task.id} removeTask={removeTask} completeTask={completeTask}/>)}
+            {tasks.map((task) => 
+                <TaskCard 
+                task={task} 
+                key={task.id} 
+                removeTask={removeTask} 
+                completeTask={completeTask}
+                updateTask={updateTask}
+                />)}
         </div>
     )
 }
--- src/components/tasks/Taskcard.jsx
+++ src/components/tasks/Taskcard.jsx
@@ -1,10 +1,51 @@
-export default function TaskCard({task, completeTask}){
+export default function TaskCard({task, completeTask, updateTask, removeTask}){
+    const [editing, setEditing] = useState(false);
+    const [title, setTitle] = useState(task.title);
+    const [description, setDescription] = useState(task.description);
+    const [priority, setPriority] = useState(task.priority);
+
+    function handleSubmit(){ 
+        if(title === ""){ return }
+
+        updateTask({
+            id: task.id,
+            title: title,
+            description: description,
+            priority: priority,
+            completed: task.completed
+        });
+
+        setEditing(false);
+    }
     return (
         <div>
-            <p>Nome da tarefa: {task.title}</p>
-            <p>Descrição: {task.description}</p>
-            <p>Prioridade: {task.priority}</p>
-            <button onClick={() => completeTask(task.id)} disabled={task.completed}>Concluir tarefa</button>
+            {(editing) 
+                ? <input  type="text" value={title} 
+                        onChange={e => setTitle(e.target.value)} 
+                        placeholder={title} /> 
+                : <p>Nome da tarefa: {task.title}</p>}
+            
+            {(editing) 
+                ? <input  type="text" value={description} 
+                        onChange={e => setDescription(e.target.value)} 
+                        placeholder={description} /> 
+                : <p>Descrição da tarefa: {task.description}</p>}
+
+            {(editing) 
+                ?   <select value={priority} onChange={e => setPriority(e.target.value)}>
+                        <option value="baixa">Baixa</option>
+                        <option value="alta">Alta</option>
+                    </select>
+                : <p>Prioridade: {task.priority}</p>}
+
+            {(editing === false) && <button onClick={() => setEditing(true)}>Editar</button>}
+            {(editing) && <button onClick={() => handleSubmit()}>Concluir edição</button>}
+            
+            {(editing === false) && 
+            <button onClick={() => completeTask(task.id)} disabled={task.completed}>
+                Concluir tarefa
+            </button>}
+
             <button onClick={() => removeTask(task.id)}>Excluir tarefa</button>
         </div>
     )
